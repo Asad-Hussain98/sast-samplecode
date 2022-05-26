4d7ef07f0a8fb195c40e4c6066bfa05199f33213
Asad-Hussain98 <97272912+Asad-Hussain98@users.noreply.github.com>
2022-05-26 15:38:30 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+# Python program to find the
+# maximum of two numbers
+  
+  
+def maximum(a, b):
+      
+    if a >= b:
+        return a
+    else:
+        return b
+      
+# Driver code
+a = 2
+b = 4
+print(maximum(a, b))
+
+
+# Python program to find the
+# maximum of two numbers
+      
+# Driver code
+a = 2
+b = 4
+  
+# Use of ternary operator
+print(a if a >= b else b)
+  
+# This code is contributed by AnkThon
